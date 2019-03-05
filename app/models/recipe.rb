69b635aea499c05e922b23995e56e863141c14e5
Peter Jang <peter.x.jang@gmail.com>
2019-03-04 18:34:17 -0600

    Add model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,26 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    result = []
+    ingredients.split(", ").each do |ingredient|
+      result << ingredient.downcase
+    end
+    result
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hour(s) " if hours > 0
+    result += "#{minutes} minute(s)" if minutes > 0
+    result
+  end
 end
--- app/views/api/recipes/index.json.jbuilder
+++ app/views/api/recipes/index.json.jbuilder
@@ -3,6 +3,8 @@ json.array! @recipes.each do |recipe|
   json.title recipe.title
   json.chef recipe.chef
   json.prep_time recipe.prep_time
-  json.ingredients recipe.ingredients
-  json.directions recipe.directions
+  json.ingredients recipe.ingredients_list
+  json.directions recipe.directions_list
+  json.created_at recipe.friendly_created_at
+  json.friendly_prep_time recipe.friendly_prep_time
 end
