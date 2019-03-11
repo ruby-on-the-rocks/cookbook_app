c47c66934bcc12fd681f7a66cac1cc465cc4cb6c
Peter Jang <peter.x.jang@gmail.com>
2019-03-11 18:35:21 -0500

    Add signup action (users create)

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   #   get "/photos" => "photos#index"
   # end
   namespace :api do
+    post "/users" => "users#create"
+    
     get "/recipes" => "recipes#index"
     post "/recipes" => "recipes#create"
     get "/recipes/:id" => "recipes#show"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_012535) do
+ActiveRecord::Schema.define(version: 2019_03_11_231840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2019_02_26_012535) do
     t.string "directions"
     t.string "image_url"
     t.integer "prep_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
