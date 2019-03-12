fde717ba37b06c249d8342295bc71eeeea96fc1e
Peter Jang <peter.x.jang@gmail.com>
2019-03-11 19:01:29 -0500

    Add login action (sessions create)

--- Gemfile
+++ Gemfile
@@ -49,3 +49,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'http'
 gem 'rack-cors'
+gem 'jwt'
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   namespace :api do
     post "/users" => "users#create"
     
+    post "/sessions" => "sessions#create"
+
     get "/recipes" => "recipes#index"
     post "/recipes" => "recipes#create"
     get "/recipes/:id" => "recipes#show"
