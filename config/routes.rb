5f8bf041a5b403fff9bcae33fdcf744eccaec76d
Peter Jang <peter.x.jang@gmail.com>
2019-03-19 19:42:36 -0500

    Add recipes HTML index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
+  get "/recipes" => "recipes#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   # namespace :api do
