701e62c652a9df0ee4dd8bdc6e5f3b67aec1ffb1
Peter Jang <peter.x.jang@gmail.com>
2019-03-31 09:16:13 -0500

    Temporarily disable authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def current_user
+    return User.first
     auth_headers = request.headers['Authorization']
     if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
       token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
